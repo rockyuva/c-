4bbc7df6e698b25180ab9081d818b62d4f4129d6
rockyuva <113331128+rockyuva@users.noreply.github.com>
2022-09-13 13:58:12 +0530

    Add files via upload

---
+++ valid or not.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string s1;
+	cin>>s1;
+	string s2;
+	cin>>s2;
+	if( s1==s2)
+	cout<<"both are equal";
+	else
+	cout<<"both are not equal";
+}
