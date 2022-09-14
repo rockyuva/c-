758d61ddccfe33dac6e04b98bd7f58af610c1a7a
rockyuva <113331128+rockyuva@users.noreply.github.com>
2022-09-14 14:21:12 +0530

    Add files via upload

---
+++ data type.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+using namespace std;
+class printnumber
+{
+	int n1;
+	char a[20];
+	float n2;
+	double n3;
+	long int n4;
+public:
+	printnumber(int);
+	printnumber(char);
+	printnumber(float);
+	printnumber(double);
+	printnumber(long double);
+}
+printnumber::printnumber(int a)
+{
+	cout<<"Integer"<<a;
+}
+printnumber::printnumber(char b)
+{
+	cout<<"Character"<<b;
+}
+printnumber::printnumber(float c)
+{
+	cout<<"Float"<<c;
+}
+printnumber::printnumber(double d)
+{
+	cout<<"Double"<<d;
+}
+printnumber::printnumber(long double e )
+{
+	cout<<Long Double<<e
+}
+#include<iostream>
+using namespace std;
+class printnumber
+{
+	int n1;
+	char a[20];
+	float n2;
+	double n3;
+	long int n4;
+public:
+	printnumber(int);
+	printnumber(char);
+	printnumber(float);
+	printnumber(double);
+	printnumber(long double);
+}
+printnumber::printnumber(int a)
+{
+	cout<<"Integer"<<a;
+}
+printnumber::printnumber(char b)
+{
+	cout<<"Character"<<b;
+}
+printnumber::printnumber(float c)
+{
+	cout<<"Float"<<c;
+}
+printnumber::printnumber(double d)
+{
+	cout<<"Double"<<d;
+}
+printnumber::printnumber(long double e )
+{
+	cout<<Long Double<<e
+}
+#include<iostream>
+using namespace std;
+class printnumber
+{
+	int n1;
+	char a[20];
+	float n2;
+	double n3;
+	long int n4;
+public:
+	printnumber(int);
+	printnumber(char);
+	printnumber(float);
+	printnumber(double);
+	printnumber(long double);
+}
+printnumber::printnumber(int a)
+{
+	cout<<"Integer"<<a;
+}
+printnumber::printnumber(char b)
+{
+	cout<<"Character"<<b;
+}
+printnumber::printnumber(float c)
+{
+	cout<<"Float"<<c;
+}
+printnumber::printnumber(double d)
+{
+	cout<<"Double"<<d;
+}
+printnumber::printnumber(long double e )
+{
+	cout<<Long Double<<e
+}
+
+
+
+
